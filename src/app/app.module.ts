e61f738ca35f47a92b5412620485aa5e0fcc733f
Daniel lara <daniellaraedinburgh@hotmail.com>
2018-09-30 03:23:41 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { HomeComponent } from './public/home/home.component';
+import { VehiclesComponent } from './public/vehicles/vehicles.component';
+import { VehiclesService } from './public/vehicles/services/vehicles.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GetImageUrlPipe } from './common/pipes/get-image-url.pipe';
+import { LoginFormComponent } from './public/login/login-form/login-form.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { AuthHomeComponent } from './auth/auth-home/auth-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    VehiclesComponent,
+    GetImageUrlPipe,
+    LoginFormComponent,
+    AuthHomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [VehiclesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
